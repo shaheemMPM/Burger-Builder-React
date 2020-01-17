aeddc193d4a4f254f7d655d528a977c7b7e2231c
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-17 17:07:42 +0530

    Make Burger Dynamic Step: 1

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,23 @@ import './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient 
+                            key = {igKey+i}
+                            type = {igKey}/>
+                })
+        })
+
+    console.log(transformedIngredients);
+    
+
     return ( 
         <div className="Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
      );
