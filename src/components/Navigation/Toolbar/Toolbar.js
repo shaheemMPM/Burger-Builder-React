65d9666ddfc49c2a432208175cf30e1c476d9859
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-18 19:34:31 +0530

    Create Custom Toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Toolbar.css';
+
+const toolbar = (props) => {
+    return ( 
+        <header className="Toolbar">
+            <div>MENU</div>
+            <div>LOGO</div>
+            <nav>
+                ...
+            </nav>
+        </header>
+     );
+}
+ 
+export default toolbar;
