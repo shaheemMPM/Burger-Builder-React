9436f7e5171c88d6aad31fa6149f51507caff82b
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-19 08:45:00 +0530

    Add NavigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './NavigationItem.css';
+
+const navigationItem = (props) => {
+  return (
+    <li className="NavigationItem">
+      <a
+        href={props.link}
+        className={props.active ? 'active' : ''}>
+          {props.children}
+      </a>
+    </li>
+  );
+}
+
+export default navigationItem;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems.js';
 
 const toolbar = (props) => {
-    return ( 
+    return (
         <header className="Toolbar">
             <div>MENU</div>
             <Logo />
             <nav>
-                ...
+                <NavigationItems />
             </nav>
         </header>
      );
 }
- 
+
 export default toolbar;
