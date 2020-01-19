b8d3deec231b1cb07bdeefbde2ad9dc01c77588a
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-19 15:13:25 +0530

    Improve Performance : remove unwanted rendering of Modal and OrderSummary (Make Modal A class)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
+import './OrderSummary.css';
 
 const orderSummary = ( props ) => {
     const ingredientSummary = Object.keys( props.ingredients )
@@ -16,7 +17,7 @@ const orderSummary = ( props ) => {
         <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
-            <ul>
+            <ul className="IngredientsList">
                 {ingredientSummary}
             </ul>
             <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
